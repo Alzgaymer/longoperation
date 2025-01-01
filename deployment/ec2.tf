02926e9607f7c8bc2ddd16fb5d6bdbc27afb08be
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-01-01 16:25:04 +0200

    terraform: right ami and instance type

--- deployment/ec2.tf
+++ deployment/ec2.tf
@@ -1,5 +1,26 @@
 resource "aws_instance" "hello_world" {
-  ami           = "ami-02df5cb5ad97983ba"
+  ami           = "ami-0f174d97d7d7a029b"
   instance_type = "t3.micro"
   key_name = "terraform"
+  tags = {
+    Name = "hello_world"
+  }
+  security_groups = [aws_security_group.ssh.name]
+}
+
+resource "aws_security_group" "ssh" {
+  name = "hello_world_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
