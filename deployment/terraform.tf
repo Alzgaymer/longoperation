5506c687f9ad05ae19f2e69e19e3f63e72d5b3bb
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-02-09 13:43:50 +0200

    terraform

--- deployment/terraform.tf
+++ deployment/terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+      version = "~> 5.86.0"        # Use a version of the AWS provider that is compatible with version
     }
   }
 }
