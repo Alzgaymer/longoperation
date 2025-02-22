4161900a882a430eda488f7124afc38ab982826a
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-02-22 22:37:18 +0200

    actions

--- deployment/api-server-fargate.tf
+++ deployment/api-server-fargate.tf
@@ -11,21 +11,21 @@ resource "aws_ecs_task_definition" "api-long-operation" {
   memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = <<TASK_DEFINITION
-[
-  {
-    "name": "api-long-operation",
-    "image": "${aws_ecr_repository.oci_registry.repository_url}/api-long-operation:latest",
-    "cpu": 256,
-    "memory": 512,
-    "portMappings": [
-      {
-        "containerPort": 80,
-        "hostPort": 80,
-        "protocol": "tcp"
-      }
-    ],
-    "user": "server"
-  }
-]
-TASK_DEFINITION
+  [
+    {
+      "name": "api-long-operation",
+      "image": "${aws_ecr_repository.oci_registry.repository_url}/api-long-operation:latest",
+      "cpu": 256,
+      "memory": 512,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80,
+          "protocol": "tcp"
+        }
+      ],
+      "user": "server"
+    }
+  ]
+  TASK_DEFINITION
 }
