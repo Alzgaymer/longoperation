eaded3ca30502cfaa1e15a2de40fa04f53e55dc7
Alzgaymer <43280554+Alzgaymer@users.noreply.github.com>
2025-02-22 19:44:10 +0000

    Apply automatic changes

--- deployment/api-server-fargate.tf
+++ deployment/api-server-fargate.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "api-long-operation" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn =      aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = <<TASK_DEFINITION
 [
   {
