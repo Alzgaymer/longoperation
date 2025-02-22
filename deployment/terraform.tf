f4d8eee8101b02d2aa65e5bbe1a37cdc5ae1b72c
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-02-22 18:32:26 +0200

    terraform remote state

--- deployment/terraform.tf
+++ deployment/terraform.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 5.86.0"     # Use a version of the AWS provider that is compatible with version
     }
   }
+
+  backend "s3" {
+    encrypt = true
+    bucket = "alzgaymer-terraform"
+    key    = "api/long-operation/terraform.tfstate"
+    region = "eu-north-1"
+  }
 }
 
 provider "aws" {
