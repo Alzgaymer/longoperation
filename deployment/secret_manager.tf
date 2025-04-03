5d6adb0db70362d68f93b865c588b32e701dafd8
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-04-03 16:44:21 +0300

    feat(terraform): refactor MongoDB secrets management to use local variables for username and password

--- deployment/secret_manager.tf
+++ deployment/secret_manager.tf
@@ -1,19 +1,27 @@
-module "secrets_manager" {
-  source = "terraform-aws-modules/secrets-manager/aws"
-
-  name_prefix   = "MONGO_"
-  name          = "USERNAME"
-  description   = "Mongo atlas database username"
-  create_policy = true
-  secret_string = var.mongo_username
+locals {
+  mongo_secrets = {
+    USERNAME = {
+      description = "Mongo atlas database username"
+      value       = var.mongo_username
+      sensitive   = true
+    }
+    PASSWORD = {
+      description = "Mongo atlas database password"
+      value       = var.mongo_password
+      sensitive   = true
+    }
+  }
 }
 
-module "secrets_manager" {
-  source = "terraform-aws-modules/secrets-manager/aws"
+module "mongo_secrets" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.3.1"
+
+  for_each = local.mongo_secrets
 
   name_prefix   = "MONGO_"
-  name          = "PASSWORD"
-  description   = "Mongo atlas database password"
+  name          = each.key
+  description   = each.value.description
   create_policy = true
-  secret_string = var.mongo_password
+  secret_string = each.value.value
 }
