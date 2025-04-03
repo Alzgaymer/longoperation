f6bc0b34379c0800dd81974118bb2f9d2f79d85c
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-04-03 18:27:34 +0300

    feat(terraform): add policy for ECS task access to MongoDB secrets

--- deployment/secret_manager.tf
+++ deployment/secret_manager.tf
@@ -8,4 +8,19 @@ module "mongo_secrets" {
     username = var.mongo_username,
     password = var.mongo_password
   })
+
+  create_policy       = true
+  block_public_policy = true
+  policy_statements = {
+    Sid    = "AllowEcsTaskToReadSecret"
+    Effect = "Allow"
+    Action = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+    Principal = {
+      Service = "ecs-tasks.amazonaws.com"
+    }
+    Resource = "*"
+  }
 }
