c606e6882727401bb897046671de140dc6b5ff65
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-04-07 21:15:36 +0300

    feat(terraform): add security group rule for health check ingress

--- deployment/security_group.tf
+++ deployment/security_group.tf
@@ -29,3 +29,12 @@ resource "aws_security_group_rule" "allow_all" {
   security_group_id = aws_security_group.long-operation-sg.id
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "allow_healthcheck" {
+  from_port         = var.container_port
+  to_port           = var.container_port
+  protocol          = "tcp"
+  type              = "ingress"
+  security_group_id = aws_security_group.long-operation-sg.id
+  cidr_blocks       = ["10.0.0.0/16"]
+}
